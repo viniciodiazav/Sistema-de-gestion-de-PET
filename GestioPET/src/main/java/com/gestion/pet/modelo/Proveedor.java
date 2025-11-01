959e0423e12fdefa7887c09e37450c996546e7b8
Vinicio <lvdiazav@gmail.com>
2025-11-01 15:39:08 -0600

    editando archivos

--- GestioPET/src/main/java/com/gestion/pet/modelo/Proveedor.java
+++ GestioPET/src/main/java/com/gestion/pet/modelo/Proveedor.java
@@ -8,7 +8,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "provedores")
+@Table(name = "proveedores")
 public class Proveedor {
     
     @Id
--- test/testProveedores.sql
+++ test/testProveedores.sql
@@ -1,4 +1,4 @@
-INSERT INTO `gestion_pet`.`provedores` (`contacto`, `correo`, `direccion`, `nombre`, `telefono`) VALUES ('Vinicio', 'vinicio@gmail.com', 'Balboa 1111', 'PPEETT', '5535011906');
-INSERT INTO `gestion_pet`.`provedores` (`contacto`, `correo`, `direccion`, `nombre`, `telefono`) VALUES ('Eduardo', 'eduardo@gmail.com', 'Balboa 1111', 'PETyP', '5554555490');
-INSERT INTO `gestion_pet`.`provedores` (`contacto`, `correo`, `direccion`, `nombre`, `telefono`) VALUES ('Leonardo', 'leonardo@gmail.com', 'Balboa 1111', 'PETPLA', '5569668535');
-INSERT INTO `gestion_pet`.`provedores` (`contacto`, `correo`, `direccion`, `nombre`, `telefono`) VALUES ('Rafael', 'rafael@gmail.com', 'Balboa 1111', 'PETyP S.A de C.V', '5574212247');
+INSERT INTO `gestion_pet`.`proveedores` (`contacto`, `correo`, `direccion`, `nombre`, `telefono`) VALUES ('Vinicio', 'vinicio@gmail.com', 'Balboa 1111', 'PPEETT', '5535011906');
+INSERT INTO `gestion_pet`.`proveedores` (`contacto`, `correo`, `direccion`, `nombre`, `telefono`) VALUES ('Eduardo', 'eduardo@gmail.com', 'Balboa 1111', 'PETyP', '5554555490');
+INSERT INTO `gestion_pet`.`proveedores` (`contacto`, `correo`, `direccion`, `nombre`, `telefono`) VALUES ('Leonardo', 'leonardo@gmail.com', 'Balboa 1111', 'PETPLA', '5569668535');
+INSERT INTO `gestion_pet`.`proveedores` (`contacto`, `correo`, `direccion`, `nombre`, `telefono`) VALUES ('Rafael', 'rafael@gmail.com', 'Balboa 1111', 'PETyP S.A de C.V', '5574212247');
